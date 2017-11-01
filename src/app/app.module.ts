7e89465f2c2b0fc7236bc9e22798fe8349425df9
amehic9 <ajdin.mehic9@gmail.com>
2017-11-01 17:59:22 +0100

    Added basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { LearningComponent } from './learning/learning.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    QuizComponent,
+    LearningComponent
   ],
   imports: [
     BrowserModule
