83b662d352081f27e126e4b2e30180eaef9fb1ca
Fikret Basic <basic@student.tugraz.at>
2017-11-25 21:57:19 +0100

    Added starting SVG flag coloring elements
    Added the ability to color a flag by picking one of the 6 major colors. To be expanded later for the whole quiz.

--- src/app/learning/learning.component.ts
+++ src/app/learning/learning.component.ts
@@ -10,12 +10,33 @@ export class LearningComponent implements OnInit {
   continent = this.route.snapshot.params['continent'];
   constructor(private route: ActivatedRoute) { }
 
+  // SVG color
+  filterColor:boolean[] = [false, false, false, false, false, false];
+  currentColorID:number;
+  currentColor:string = "white";
+  //
+
   ngOnInit() {
     console.log(this.continent);
   }
 
   showEuropePage() {
 
+  }
+
+  pickColor(): void{  
+    this.filterColor.forEach((element, index, array) => {
+        if(index == this.currentColorID) {
+            array[index] = true;
+        }
+        else {
+            array[index] = false;
+        }
+    });
+  }
+
+  colorFlag(object): void{
+      object.target.attributes['fill'].value = this.currentColor;
   }
 
 }
