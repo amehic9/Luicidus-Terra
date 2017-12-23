e1a3ae2dfe53b81fe1445abd01d0e707a645325b
Fikret Basic <basic@student.tugraz.at>
2017-12-24 00:26:24 +0100

    REST learning
    Started adding info for REST service of the countries in the Learning page.

--- src/app/learning/learning.component.ts
+++ src/app/learning/learning.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-learning',
@@ -8,9 +9,34 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class LearningComponent implements OnInit {
   continent = this.route.snapshot.params['continent'];
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,  private http: Http) { }
+
+  countryInfo;
+  curret_country:string;
 
   anthem_file:string = "assets/audio/Austria.mp3";
+
+  getCountries() {
+    this.getCountriesJson()
+      .subscribe(
+        response => {
+          let responseJson = response.json();
+          this.countryInfo = [];
+
+          this.countryInfo = <any>responseJson[0];
+
+          //window.alert(this.countryInfo.name);
+        }
+      )
+  }
+
+  getCountriesJson() {
+    return this.http.get("https://restcountries.eu/rest/v2/name/" + this.curret_country.toLowerCase());
+    //return this.http.get("https://restcountries.eu/rest/v2/alpha/col");
+
+    // https://github.com/fayder/restcountriess
+  }
+
 
   ngOnInit() {
     console.log(this.continent);
@@ -28,6 +54,9 @@ export class LearningComponent implements OnInit {
 
   setAnthem(country) {
     this.anthem_file = "assets/audio/" + country + ".mp3";
+    this.curret_country = country;
+
+    this.getCountries();
 
     this.play();
   }
